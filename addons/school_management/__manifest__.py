c42cea1088b276daeb40242a1d09bfb9283389fd
tarunsai <kakarla.sai3@gmail.com>
2024-07-11 10:41:11 +0530

    added new file

---
+++ addons/school_management/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'School Management',
+    'version': '1.0',
+    'category': 'Education',
+    'summary': 'Manage school students',
+    'description': """
+    This module helps you to manage school students.
+    """,
+    'author': 'Your Name',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/menu.xml',
+        'views/school.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
